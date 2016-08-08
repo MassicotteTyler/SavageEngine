1c5f990e9b92c53e27c3cf483cac6c130fa18856
Massicottetyler <massicottetyler@gmail.com>
2016-08-08 00:10:36 -0700

    tested transforms

--- src/Engine/Renderable.js
+++ src/Engine/Renderable.js
@@ -8,7 +8,7 @@ Renderable.prototype.draw = function(modelTransform)
 {
 	var gl = gEngine.Core.getGL();
 	this.mShader.activateShader(this.mColor);
-  this.mShader.loadObjectTransform(modelTransform);
+  this.mShader.loadObjectTransform(this.mXform.getXform());
 	gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
 };
 
