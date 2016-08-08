97dd4e7c2b509bf9f85cdc1d75e2d137fd9dc65f
MassicotteTyler <massicottetyler@gmail.com>
2016-08-07 21:21:22 -0700

    Folder restructering, added Renderable.js

---
+++ src/Engine/Renderable.js
@@ -0,0 +1,17 @@
+function Renderable(shader)
+{
+	this.mShader = shader;
+	this.mColor = [1, 1, 1, 1];
+}
+
+Renderable.prototype.draw = function()
+{
+	var gl = gEngine.Core.getGL();
+	this.mShader.activateShader(this.mColor);
+	gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+};
+
+Renderable.prototype.setColor = function(color) { this.mColor = color; };
+Renderable.prototype.getColor = function() { return this.mColor; };
+
+
